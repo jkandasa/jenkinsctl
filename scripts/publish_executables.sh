0e99681a7a08a65e8273065e02d17a775e969379
Jeeva Kandasamy <jkandasa@gmail.com>
2021-06-21 23:26:54 +0530

    LICENSE
    Signed-off-by: Jeeva Kandasamy <jkandasa@gmail.com>

--- scripts/publish_executables.sh
+++ scripts/publish_executables.sh
@@ -7,15 +7,34 @@
 source ./scripts/version.sh
 
 BUILD_DIR=builds
+BINARY_DIR=binary
 # clean builds directory
 rm ${BUILD_DIR}/* -rf
 
-
 # create directories
-mkdir -p ${BUILD_DIR}
+mkdir -p ${BUILD_DIR}/${BINARY_DIR}
 
 # download dependencies
 go mod tidy
+
+function package {
+  local PACKAGE_STAGING_DIR=$1
+  local BINARY_FILE=$2
+  local FILE_EXTENSION=$3
+
+  mkdir -p ${PACKAGE_STAGING_DIR}
+
+  # echo "Package dir: ${PACKAGE_STAGING_DIR}"
+  cp ${BUILD_DIR}/${BINARY_DIR}/${BINARY_FILE} ${PACKAGE_STAGING_DIR}/jenkinsctl${FILE_EXTENSION}
+
+  # copy license
+  cp LICENSE ${PACKAGE_STAGING_DIR}/LICENSE.txt
+
+  ARCHIVE_NAME="${PACKAGE_STAGING_DIR}.tar.gz"
+  # echo "Packaging into: ${ARCHIVE_NAME}"
+  tar -czf ${BUILD_DIR}/${ARCHIVE_NAME} ${PACKAGE_STAGING_DIR}
+  rm ${PACKAGE_STAGING_DIR} -rf
+}
 
 
 # platforms to build
@@ -27,19 +46,19 @@ do
   platform_raw=(${platform//\// })
   GOOS=${platform_raw[0]}
   GOARCH=${platform_raw[1]}
+  package_name="jenkinsctl-${GOOS}-${GOARCH}"
+
+  env GOOS=${GOOS} GOARCH=${GOARCH} CGO_ENABLED=0 go build -o ${BUILD_DIR}/${BINARY_DIR}/${package_name} -ldflags "$LD_FLAGS" cmd/main.go
+  if [ $? -ne 0 ]; then
+    echo 'an error has occurred. aborting the build process'
+    exit 1
+  fi
 
   FILE_EXTENSION=""
   if [ $GOOS = "windows" ]; then
     FILE_EXTENSION='.exe'
   fi
 
-  package_name="jenkinsctl-${VERSION}-${GOOS}-${GOARCH}${FILE_EXTENSION}"
-
-  env GOOS=${GOOS} GOARCH=${GOARCH} CGO_ENABLED=0 go build -o ${BUILD_DIR}/${package_name} -ldflags "$LD_FLAGS" cmd/main.go
-  if [ $? -ne 0 ]; then
-    echo 'an error has occurred. aborting the build process'
-    exit 1
-  fi
+  package jenkinsctl-${VERSION}-${GOOS}-${GOARCH} ${package_name} ${FILE_EXTENSION}
 
 done
-
